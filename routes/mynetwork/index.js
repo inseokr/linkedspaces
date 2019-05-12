d0c95890b1e5ccd84955e0ec2c269938553d1188
inseokr <inseo.kr@gmail.com>
2019-05-11 23:23:22 -0700

    my_network: pass parameters from route to ejs

--- routes/mynetwork/index.js
+++ routes/mynetwork/index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var User    = require("../../models/user");
 var router  = express.Router();
 
 var node = require("deasync");
@@ -6,8 +7,31 @@ var node = require("deasync");
 node.loop = node.runLoopOnce;
 
 
-router.get("/", function(req,res){
-	res.render("mynetwork/mynetwork_main");
+router.get("/", function(req, res){
+
+	// 1. read user dabase and construct friends_list
+	// <note> let's go through the full list for now.
+	User.find({}, function(error, users) {
+		var friends_list = [];
+
+		users.forEach(function(user){
+			if(req.user._id.equals(user._id)!=true)
+			{
+				var friend = {profile_picture: "../public/user_resources/pictures/Chinh - Vy.jpg", 
+				              name: user.firstname + user.lastname, 
+				              address: {city: "San Jose", state: "CA"}};
+				friends_list.push(friend);
+			}
+		});
+
+		res.render("mynetwork/mynetwork_main", {friends_list: friends_list});
+	})
+
+	//var friends_list = [];
+	//var chinh = {profile_picture: "../public/user_resources/pictures/Chinh - Vy.jpg", name: "Chinh Le", address: {city: "San Jose", state: "CA"} } ;
+	//friends_list.push(chinh);
+	//var peter = {profile_picture: "../public/user_resources/pictures/Peter.jpg", name: "Peter Bae", address: {city: "Cupertino", state: "CA"} } ;
+	//friends_list.push(peter);
 });
 
 router.get("/:filename", function(req, res){
