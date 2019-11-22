37858599101c5ca22c6e0796245c96702c338fbf
Justin Seo <justinseo@Justins-MacBook-Pro-2.local>
2019-11-21 23:38:52 -0800

    Animated transition for markers. Doesn't work well but reference for later development.

--- react_poc/client/src/containers/LandingPage/index.jsx
+++ react_poc/client/src/containers/LandingPage/index.jsx
@@ -4,6 +4,7 @@ import ListView from "./ListView";
 import Grid from "@material-ui/core/Grid";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Box from '@material-ui/core/Box'
+import ListingInfo from "./ListingInfo";
 
 class LandingPage extends Component {
     constructor(props) {
@@ -17,29 +18,91 @@ class LandingPage extends Component {
             labelStyle: {
                 fontSize: 20
             },
-            testList: [{"name":"Peter Brimer"}, {"name":"Tera Gaona"}, {"name":"Kandy Liston"}]
+            testList: [{"address":"793 Praderia Circle, Fremont, CA, 94539", "price":"1000 Per Month","duration":"12 Months","coordinates":{"lat":37.519200,"lng":-121.923760}},
+                       {"address":"43349 Pacific Commons Blvd, Fremont, CA 94538", "price":"2500 Per Month","duration":"6 Months","coordinates":{"lat":37.503460,"lng":-121.972880}},
+                       {"address":"1011 Pacific Ave, Santa Cruz, CA 95060", "price":"500 Per Month","duration":"3 Months","coordinates":{"lat":36.971300,"lng":-122.025610}},
+                       {"address":"99 Grove St, San Francisco, CA 94102", "price":"2300 Per Month","duration":"8 Months","coordinates":{"lat":37.778080,"lng":-122.417320}},
+                       {"address":"1417 R St, Sacramento, CA 95811", "price":"700 Per Month","duration":"6 Months","coordinates":{"lat":38.571850,"lng":-121.498627}},
+                       {"address":"3333 Scott Blvd Santa Clara, CA 95054", "price":"5000 Per Month","duration":"12 Months","coordinates":{"lat":37.382439,"lng":-121.982117}},
+                       {"address":"2610 Orchard Parkway San Jose , CA 95134", "price":"2100 Per Month","duration":"4 Months","coordinates":{"lat":37.385660,"lng":-121.931020}},
+                       {"address":"665 W Jefferson Blvd, Los Angeles, CA 90007", "price":"4000 Per Month","duration":"21 Months","coordinates":{"lat":34.023987,"lng":-118.281273}},
+                       {"address":"1 Amphitheatre Pkwy, Mountain View, CA 94043", "price":"200 Per Month","duration":"1 Month","coordinates":{"lat":37.426804,"lng":-122.080620}},
+                       {"address":"886 Cannery Row, Monterey, CA 93940", "price":"500 Per Month","duration":"15 Months","coordinates":{"lat":36.618149,"lng":-121.901939}}],
+            listing_info_opened: false
         };
+        this.UpdateCenterFromChild = this.UpdateCenterFromChild.bind(this);
+        this.ToggleListingInfoFromChild = this.ToggleListingInfoFromChild.bind(this);
+    }
+
+    UpdateCenterFromChild(listing) {
+        let coordinates = [];
+        let lat1 = this.state.center.lat, lng1 = this.state.center.lng;
+        let lat2 = listing.coordinates.lat, lng2 = listing.coordinates.lng;
+        // If initial position is to the left of the destination position according to a +/- number line then true, else false.
+        let incrementLat = lat1 < lat2 ? 0.1 : -0.1;
+        let incrementLng = lng1 < lng2 ? 0.1 : -0.1;
+        let updatedLat = lat1, updatedLng = lng1;
+        console.log("lat1,lng1",lat1,lng1);
+        console.log("lat2,lng2",lat2,lng2);
+        while (updatedLat !== lat2 || updatedLng !== lng2) { // This while loop will increment/decrement the center until it reaches its desired destination.
+            console.log("lmao",updatedLat,updatedLng);
+            // updatedLat = ((updatedLat + incrementLat) < lat2) ? (updatedLat + incrementLat) : lat2;
+            // updatedLng = ((updatedLng + incrementLng) < lng2) ? (updatedLng + incrementLng) : lng2;
+            if (incrementLat) { // Increment
+                updatedLat += 0.1;
+                updatedLat = (updatedLat < lat2) ? updatedLat : lat2;
+            } else {
+                updatedLat -= 0.1;
+                updatedLat = (updatedLat < lat2) ? updatedLat : lat2;
+            }
+            if (incrementLng) { // Increment
+                updatedLng += 0.1;
+                updatedLng = (updatedLng < lng2) ? updatedLng : lng2;
+            } else {
+                updatedLng -= 0.1;
+                updatedLng = (updatedLng > lng2) ? updatedLng : lng2;
+            }
+            // coordinates.push({"lat":updatedLat,"lng":updatedLng});
+        }
+        console.log(coordinates);
+        // setTimeout(() => {
+        //     this.setState({
+        //         center: {
+        //             lat:updatedLat,
+        //             lng:updatedLng
+        //         }
+        //     });
+        //     console.log("HI",updatedLat,updatedLng);
+        // }, 1000);
+    }
+
+    ToggleListingInfoFromChild(){
+        this.setState({
+            listing_info_opened: !this.state.listing_info_opened
+        });
     }
 
     render() {
-        const {center,zoom,labelStyle,testList} = this.state;
+        const {center,zoom,labelStyle,testList,listing_info_opened} = this.state;
         return (
             <Grid component="main">
                 <CssBaseline />
                 <Box className="App" component="div" display="flex" flexDirection="column">
                     <Grid container>
                         <Grid item xs={4}>
-                            <ListView testList={testList}/>
+                            <ListView testList={testList} updateCenter={this.UpdateCenterFromChild} toggleListingInfo={this.ToggleListingInfoFromChild}/>
+                        </Grid>
+                        <Grid item>
+                            <ListingInfo listing_info_opened={listing_info_opened} toggleListingInfo={this.ToggleListingInfoFromChild}/>
                         </Grid>
                         <Grid className="map" item xs={8}>
-                            <MapView center={center} zoom={zoom} labelStyle={labelStyle}/>
+                            <MapView center={center} zoom={zoom} labelStyle={labelStyle} testList={testList} toggleListingInfo={this.ToggleListingInfoFromChild}/>
                         </Grid>
                     </Grid>
                 </Box>
             </Grid>
         );
     }
-
 }
 
 export default LandingPage;
